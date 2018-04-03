387164a2249335f65a3130b8644ad7cc5c8cd9fa
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-03 15:55:14 +0200

    Update main.cpp
    Unified SignalHandler Function Name

--- Mindstorm/Adoptoro_Roboto/main.cpp
+++ Mindstorm/Adoptoro_Roboto/main.cpp
@@ -14,7 +14,7 @@ BrickPi3 BP;
 
 int main(){
 
-	signal(SIGINT, exit_signal_handler); // Register the exit function for Ctrl+C
+	signal(SIGINT, exitSignalHandler); // Register the exit function for Ctrl+C
 	
 	BP.detect(); // Make sure that thye BrickPi3 is communicating and that the firmware is compatible with the drivers
 	
