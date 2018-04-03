33b57663f7d3700ed3444c5a162ee48e165f973b
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-03 15:46:50 +0200

    Update main.cpp
    Added ;

--- Mindstorm/Adoptoro_Roboto/main.cpp
+++ Mindstorm/Adoptoro_Roboto/main.cpp
@@ -16,7 +16,7 @@ int main(){
 
 	signal(SIGINT, exit_signal_handler); // Register the exit function for Ctrl+C
 	
-	BP.detect() // Make sure that thye BrickPi3 is communicating and that the firmware is compatible with the drivers
+	BP.detect(); // Make sure that thye BrickPi3 is communicating and that the firmware is compatible with the drivers
 	
 	while(true){
 		sensorCalibration();
