105efbf02d00c964748054050b99968af47bce74
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-03 16:21:18 +0200

    Update calibration.cpp
    BP naming changed

--- Mindstorm/Adoptoro_Roboto/calibration.cpp
+++ Mindstorm/Adoptoro_Roboto/calibration.cpp
@@ -5,22 +5,24 @@ using namespace std;
 
 int error;
 
+BrickPi BP2;
+
 void sensorCalibration(){
 	
 	int BWValue = 0;
 	int CValue = 0;
 
-	BP.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_LIGHT_ON); // Blacc/White Sensor Port Defenition
-	BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_COLOR_FULL);	// Color Sensor Port Defenition
+	BP2.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_LIGHT_ON); // Blacc/White Sensor Port Defenition
+	BP2.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_COLOR_FULL);	// Color Sensor Port Defenition
 	
 	sensor_light_t		Light;
 	sensor_color_t		Color;
 
 	error = 0;
-	if(BP.get_sensor(PORT_2, Light) == 0){
+	if(BP2.get_sensor(PORT_2, Light) == 0){
 		BWValue = Light.reflected;
 	}
-	if(BP.get_sensor(PORT_3,Color) == 0){
+	if(BP2.get_sensor(PORT_3,Color) == 0){
 		CValue = Color.reflected_red;
 	}
 
