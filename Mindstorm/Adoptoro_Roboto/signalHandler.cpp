7357ea06d4674986b261c4a9100b15fb9a9f78c8
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-03 16:24:09 +0200

    Update signalHandler.cpp
    Changed name of BP

--- Mindstorm/Adoptoro_Roboto/signalHandler.cpp
+++ Mindstorm/Adoptoro_Roboto/signalHandler.cpp
@@ -3,11 +3,13 @@
 
 using namespace std;
 
+BrickPi3 BP2;
+
 // Signal handler that will be called when Ctrl+C is pressed to stop the program.
 
 void exitSignalHandler(int signo){
 	if(signo == SIGINT){
-		BP.reset_all();		// Reset everything so t here are no run-away motors.
+		BP2.reset_all();		// Reset everything so t here are no run-away motors.
 		exit(-2);
 	}
 }
