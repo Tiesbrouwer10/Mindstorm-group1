359e5e131b325e6d16b51b1363b9067f6e8ea0a8
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2018-04-04 13:50:31 +0200

    Update calibration.cpp

--- Mindstorm/Adoptoro_Roboto/calibration.cpp
+++ Mindstorm/Adoptoro_Roboto/calibration.cpp
@@ -12,8 +12,6 @@ void sensorCalibration(){
 	int BWValue;
 	int CValue;
 
-	BP2.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_LIGHT_ON); // Blacc/White Sensor Port Defenition
-	BP2.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_COLOR_FULL);	// Color Sensor Port Defenition
 	
 	sensor_light_t		Light;
 	sensor_color_t		Color;
