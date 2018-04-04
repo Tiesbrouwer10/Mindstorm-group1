8845f117a8919c235eb808ae560efea6b20256da
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2018-04-04 13:51:33 +0200

    Update main.cpp

--- Mindstorm/Adoptoro_Roboto/main.cpp
+++ Mindstorm/Adoptoro_Roboto/main.cpp
@@ -18,6 +18,8 @@ int main(){
 	
 	BP.detect(); // Make sure that thye BrickPi3 is communicating and that the firmware is compatible with the drivers
 	
+	BP2.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_LIGHT_ON); // Blacc/White Sensor Port Defenition
+	BP2.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_COLOR_FULL);	// Color Sensor Port Defenition
 	while(true){
 		sensorCalibration();
 	}
