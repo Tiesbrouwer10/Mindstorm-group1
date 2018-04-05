103a4d02150f3c62b7a63d46fb57a16f77e99202
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-05 11:58:30 +0200

    Update signalHandler.cpp

--- Mindstorm/Adoptoro_Roboto/signalHandler.cpp
+++ Mindstorm/Adoptoro_Roboto/signalHandler.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-BrickPi3 BP;
+BrickPi3 BPMain;
 
 // Signal handler that will be called when Ctrl+C is pressed to stop the program.
 
 void exitSignalHandler(int signo){
 	if(signo == SIGINT){
-		BP.reset_all();		// Reset everything so t here are no run-away motors.
+		BPMain.reset_all();		// Reset everything so t here are no run-away motors.
 		exit(-2);
 	}
 }
