bd796652d4d9c43b851439a4fe24aea0be4dbef4
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-05 16:03:12 +0200

    Update linerider.cpp

--- Mindstorm/Adoptoro_Roboto/linerider.cpp
+++ Mindstorm/Adoptoro_Roboto/linerider.cpp
@@ -29,15 +29,14 @@ void lineRider(borderValues calibratedInputs){
 			BWLine = Light.reflected;
 			if(BPLine.get_sensor(PORT_3,Color) == 0){
 				CLine = Color.reflected_red;
-				cout << "BW SCANNED: " << Light.reflected << " BWVALUE = " << calibratedInputs.borderValueBW << '\n';
-				cout << "C  SCANNED: " << Color.reflected_red << "  CVALUE = " << calibratedInputs.borderValueC << '\n';
+			
 				// Ride for 1 step
 				if(BWLine > calibratedInputs.borderValueBW){
 					BPLine.set_motor_power(PORT_C, BWBrake);
 					BPLine.set_motor_power(PORT_B, BWAccelerator);
 					
-					if(BWAccelerator <= 125 && BWBrake >= 0){
-							BWAccelerator += 4;
+					if(BWAccelerator <= 125){
+							BWAccelerator += 2;
 							//BWBrake -= 1;
 					}
 				}
@@ -45,8 +44,8 @@ void lineRider(borderValues calibratedInputs){
 					BPLine.set_motor_power(PORT_B, CBrake);
 					BPLine.set_motor_power(PORT_C, CAccelerator);
 					
-					if(CAccelerator <= 125 && CBrake >= 0){
-						CAccelerator += 4;
+					if(CAccelerator <= 125){
+						CAccelerator += 2;
 						//CBrake -= 1;
 					}
 				}
