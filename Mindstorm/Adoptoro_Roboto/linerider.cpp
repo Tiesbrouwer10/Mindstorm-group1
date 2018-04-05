0ea2a7533e46aa749284c0ffaa5f7149fb6a305b
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-05 13:38:31 +0200

    Update linerider.cpp

--- Mindstorm/Adoptoro_Roboto/linerider.cpp
+++ Mindstorm/Adoptoro_Roboto/linerider.cpp
@@ -13,23 +13,24 @@ void lineRider(borderValues calibratedInputs){
 	
 	while(true){
 		// Get value from sensors
-		BPLine.get_sensor(PORT_2, Light);
-		BPLine.get_sensor(PORT_3,Color);
-		cout << "BW SCANNED: " << Light.reflected << " BWVALUE = " << calibratedInputs.borderValueBW << '\n';
-		cout << "C  SCANNED: " << Color.reflected_red << "  CVALUE = " << calibratedInputs.borderValueC << '\n';
-		sleep(1);
-		cout << "SLEEP DONE\n";
-		// Ride for 1 step
-		if(Light.reflected > calibratedInputs.borderValueBW){
-			BPLine.set_motor_power(PORT_C, 10);
-		}
-		else if(Color.reflected_red < calibratedInputs.borderValueC){
+		if(BPLine.get_sensor(PORT_2, Light) == 0){
+			if(BPLine.get_sensor(PORT_3,Color) == 0){
+				cout << "BW SCANNED: " << Light.reflected << " BWVALUE = " << calibratedInputs.borderValueBW << '\n';
+				cout << "C  SCANNED: " << Color.reflected_red << "  CVALUE = " << calibratedInputs.borderValueC << '\n';
+				sleep(1);
+				cout << "SLEEP DONE\n";
+				// Ride for 1 step
+				if(Light.reflected > calibratedInputs.borderValueBW){
+					BPLine.set_motor_power(PORT_C, 10);
+				}
+				else if(Color.reflected_red < calibratedInputs.borderValueC){
 			BPLine.set_motor_power(PORT_B, 10);
+				}
+				else{
+					BPLine.set_motor_power(PORT_C, 20);
+					BPLine.set_motor_power(PORT_B, 20);
+				}
+			}
 		}
-		else{
-			BPLine.set_motor_power(PORT_C, 20);
-			BPLine.set_motor_power(PORT_B, 20);
-		}
-		
 	}
 }
