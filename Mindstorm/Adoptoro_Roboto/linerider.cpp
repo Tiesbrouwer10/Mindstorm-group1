5534d15543f462c706d9b2d33250c6ae8a38efa8
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-06 14:15:18 +0200

    Update linerider.cpp

--- Mindstorm/Adoptoro_Roboto/linerider.cpp
+++ Mindstorm/Adoptoro_Roboto/linerider.cpp
@@ -27,11 +27,11 @@ void lineRider(borderValues calibratedInputs){
 				cout << "C  SCANNED: " << Color.reflected_red << "  CVALUE = " << calibratedInputs.borderValueC << '\n';
 				// Ride for 1 step
 				if(BWLine > calibratedInputs.borderValueBW){
-					BPLine.set_motor_power(PORT_C, -40);
+					BPLine.set_motor_power(PORT_C, 0);
 					BPLine.set_motor_power(PORT_B, 60);
 				}
 				else if(CLine < calibratedInputs.borderValueC){
-					BPLine.set_motor_power(PORT_B, -40);
+					BPLine.set_motor_power(PORT_B, 0);
 					BPLine.set_motor_power(PORT_C, 60);
 				}
 				else{
