1c76a0041a0aa1f19bd8769c3aa4c83bea7d5d3a
Maerecque <33025408+Maerecque@users.noreply.github.com>
2018-04-09 12:29:02 +0200

    Create sound.cpp
    file for sound during driving

---
+++ Mindstorm/Adoptoro_Roboto/sound.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void sound(){
+/
+*before this function is usable you need to install mplayer on your linux system with the command: "sudo apt-get install mplayer"
+*and a mp3 file that you want to be played
+*this function tells the command line interface to execute a command that reads "mplayer beep.mp3" where beep.mp3 the name is of
+*the mp3 file that you want to be played
+/
+
+    system("mplayer beep.mp3");
+    
+}
