af8670b82883501af5e26b7001a4ca81d0b199c1
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-09 15:37:43 +0200

    Update linerider.cpp

--- Mindstorm/Adoptoro_Roboto/linerider.cpp
+++ Mindstorm/Adoptoro_Roboto/linerider.cpp
@@ -1,6 +1,9 @@
 #include "AdoptoroLijn.h"
 #include "BrickPi3.h"
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+
 using namespace std;
 
 const int PORT_b = 2;
@@ -8,7 +11,8 @@ const int PORT_c = 4;
 
 int acceleration_B = 0;
 int acceleration_C = 0;
-int getAcceleration(int &motor, int8_t do_up_down=0, bool set=false)
+
+int getAcceleration(const int &motor, int do_up_down=0, bool set=false)
 {
 	//0x02 = B
 	//0x04 = C
@@ -64,30 +68,35 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 				// Ride for 1 step
 				if(BWLine > calibratedInputs.borderValueBW){ //0-1000 (x1.25) (de-amplifying)
 					//moet naar rechts.
-					int acc = getAcceleration(PORT_b, 0, false);
+					int acc = getAcceleration(PORT_b);
 					if(acc > 89){
-						BPLine.set_motor_power(PORT_C, getAcceleration(PORT_c, -90, true)); //draaien om as (reverse andere wiel)
-					}else{
-						BPLine.set_motor_power(PORT_C, getAcceleration(PORT_c, 15, true)); //zet linker motor trager
-						BPLine.set_motor_power(PORT_B, getAcceleration(PORT_b, 0, false)); //standard = 40
+						BPLine.set_motor_power(PORT_C, getAcceleration(PORT_c,-90,true)); //draaien om as (reverse andere wiel)
 					}
-					getAcceleration(PORT_b, 1, false);
+					else{
+						BPLine.set_motor_power(PORT_C, getAcceleration(PORT_c,15,true)); //zet linker motor trager
+						BPLine.set_motor_power(PORT_B, getAcceleration(PORT_b)); //standard = 40
+					}
+					getAcceleration(PORT_b, 1);
+					cout << Accelerator << " BW Accelerator\n";
 					sleep(0.1);
 				}
 				else if(CLine < calibratedInputs.borderValueC){ //color sensor wijkt af.
-					int acc = getAcceleration(PORT_c, 0, false);
+					int acc = getAcceleration(PORT_C);
 					if( acc > 89){
-						BPLine.set_motor_power(PORT_B, getAcceleration(PORT_c, -90, true));
-					}else{
-						BPLine.set_motor_power(PORT_B, getAcceleration(PORT_b, 15, true));
-						BPLine.set_motor_power(PORT_B, getAcceleration(PORT_c, 0, false));
+						BPLine.set_motor_power(PORT_B, getAcceleration(PORT_c,-90,true));
 					}
-					getAcceleration(PORT_B, 1, false);
+					else{
+						BPLine.set_motor_power(PORT_B, getAcceleration(PORT_b,15,true));
+						BPLine.set_motor_power(PORT_B, getAcceleration(PORT_c));
+					}
+					getAcceleration(PORT_B, 1);
+					cout << Accelerator << " C Accelerator\n";
 					sleep(0.1);
 				}
 				else{
-					BPLine.set_motor_power(PORT_C, getAcceleration(PORT_c, 40, true));
-					BPLine.set_motor_power(PORT_B, getAcceleration(PORT_c, 40, true));
+					BPLine.set_motor_power(PORT_C, 40);
+					BPLine.set_motor_power(PORT_B, 40);
+					Accelerator = 40;
 					
 					cout << "Accelerator set to 40\n";
 				}
@@ -95,3 +104,5 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 		}
 	}
 }
+
+#pragma clang diagnostic pop
