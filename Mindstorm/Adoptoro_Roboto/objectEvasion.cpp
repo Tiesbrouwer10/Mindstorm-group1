c273278e35da8d6b8e33cd651528ef52aa143749
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-09 16:30:47 +0200

    Update objectEvasion.cpp

--- Mindstorm/Adoptoro_Roboto/objectEvasion.cpp
+++ Mindstorm/Adoptoro_Roboto/objectEvasion.cpp
@@ -16,6 +16,9 @@ void evadeObject(BrickPi3 &BPEva, borderValues &calibratedInputs){
 	
 	sensor_light_t	Light;
 	sensor_color_t	Color;
+	
+	int BWLine = 0;
+	int CLine = 0;
     
 	// Get value from sensors (temporary)
 		if(BPEva.get_sensor(PORT_2, Light) == 0 && BPEva.get_sensor(PORT_3,Color) == 0){
