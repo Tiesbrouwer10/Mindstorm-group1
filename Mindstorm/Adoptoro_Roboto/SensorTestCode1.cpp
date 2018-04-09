c27c4e476124d46f07f70ab6b0a941934cf06ca6
Tiesbrouwer10 <37109378+Tiesbrouwer10@users.noreply.github.com>
2018-04-09 13:24:31 +0200

    Update SensorTestCode1.cpp

--- Mindstorm/Adoptoro_Roboto/SensorTestCode1.cpp
+++ Mindstorm/Adoptoro_Roboto/SensorTestCode1.cpp
@@ -27,7 +27,7 @@ void ObjectInDeWeg(){
     //motor heeft 3 standen; (1) = motor aan, (0) = motor uit, (-1) = achteruit
     BPmot.set_motor_position(PORT_A, 90); //De robot draait de afstandssensor 90 gradenrichting het object
     sleep(0.2);
-    stop();
+   // stop();
 
     sleep(1);
     BPmot.set_motor_position_relative(PORT_B, -450);
