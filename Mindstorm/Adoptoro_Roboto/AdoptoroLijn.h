1fbc84c64e5e76c985cccf9142775db6ea97684c
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2018-04-10 17:38:25 +0200

    Update AdoptoroLijn.h

--- Mindstorm/Adoptoro_Roboto/AdoptoroLijn.h
+++ Mindstorm/Adoptoro_Roboto/AdoptoroLijn.h
@@ -36,7 +36,9 @@ int getDist(BrickPi3 &BPEva); // Gets the distance the robot is away from object
 
 void evadeObject(BrickPi3 &BPEva, borderValues &calibratedInputs); // Procedure to evading object
 
-void turnMotor(string port, int degrees, BrickPi3 &BPEva);
+void turnHeadLeft(BrickPi3 &BPEva);
+
+void turnCarRight(BrickPi3 &BPEva);
 
 void sound(); // Start sound effect
 
