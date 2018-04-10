877f0faea2b3f056f4e1a58f2883302f806d5078
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2018-04-10 16:32:42 +0200

    Update objectEvasion.cpp

--- Mindstorm/Adoptoro_Roboto/objectEvasion.cpp
+++ Mindstorm/Adoptoro_Roboto/objectEvasion.cpp
@@ -32,7 +32,9 @@ void evadeObject(BrickPi3 &BPEva, borderValues &calibratedInputs){
     cout << "heloo its me";
     BPEva.offset_motor_encoder(PORT_A, BPEva.get_motor_encoder(PORT_A));
     BPEva.set_motor_power(PORT_A, 10);
-    while(BPEva.get_motor_encoder(PORT_A) >90);
+    while(BPEva.get_motor_encoder(PORT_A) >90){
+	    sleep(0.01);
+    }
     BPEva.set_motor_power(PORT_A, 0);
     sleep(500);
 }
