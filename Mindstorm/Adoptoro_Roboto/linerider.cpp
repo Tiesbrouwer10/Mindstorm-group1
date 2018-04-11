0dcb3c6a5b89bb541575779814fc7124a4bc2af6
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-11 15:15:24 +0200

    Add files via upload
    Expanded object detection

--- Mindstorm/Adoptoro_Roboto/linerider.cpp
+++ Mindstorm/Adoptoro_Roboto/linerider.cpp
@@ -15,34 +15,17 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 	int BWLine = 0;
 	int CLine = 0;
 	float Accelerator = 40;
-	int distanceToObject = 0;
 	unsigned int objectTimer = 0;
 	
 	while(true){
-		// Checks if distance is within range
-		distanceToObject = getDist(BPLine);
-		cout << distanceToObject << '\n';
-		if(distanceToObject < 8 && distanceToObject > 0){
-			cout << "draai hoofd";
-			objectTimer += 1;
-			if(objectTimer > 500){
-				evadeObject(BPLine, calibratedInputs);
-			}
-		}
+	
+		// Check if object is within range
+		objectDetection(BPLine, objectTimer);
 		
-		// Get value from sensors
-		if(BPLine.get_sensor(PORT_2, Light) == 0){
-			BWLine = Light.reflected;
-			if(BPLine.get_sensor(PORT_3,Color) == 0){
-				CLine = Color.reflected_red;
-				
 				if(CLine < calibratedInputs.borderValueC && BWLine > calibratedInputs.borderValueBW){
 					BPLine.set_motor_power(PORT_C, 40);
 					BPLine.set_motor_power(PORT_B, 40);
 					Accelerator = 40;
-					if(objectTimer > 0){
-						objectTimer -= 1;
-					}
 				}
 				
 				// Ride for 1 step
@@ -57,9 +40,6 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 					BPLine.set_motor_power(PORT_C, 40);
 					BPLine.set_motor_power(PORT_B, 40);
 					Accelerator = 40;
-					if(objectTimer > 0){
-						objectTimer -= 1;
-					}
 					
 					//cout << "Accelerator set to 40\n"; 
 					
@@ -69,6 +49,19 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 	}
 }
 
+void objectDetection(BrickPi3 BPLine, unsigned int &objectTimer){
+	int distanceToObject = 0;
+	distanceToObject = getDist(BPLine);
+	if(distanceToObject < 8 && distanceToObject > 0){
+			objectTimer += 1;
+			if(objectTimer > 500){
+				evadeObject(BPLine, calibratedInputs);
+			}
+		}
+	else{
+		objectTimer -=1
+	}
+}
 
 void lineSeen(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, unsigned int &objectTimer, BrickPi3 &BPLine){
 	if(Accelerator > 69){
@@ -76,9 +69,6 @@ void lineSeen(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, uns
 	}else{
 		BPLine.set_motor_power(insideMotor, 15);
 		BPLine.set_motor_power(outsideMotor, Accelerator);
-		if(objectTimer > 0){
-			objectTimer -= 1;
-		}
 	}
 	if(Accelerator <= 70){
 		Accelerator += 0.2;
