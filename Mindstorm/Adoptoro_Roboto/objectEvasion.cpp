c31eea42eb3c31afd7243c5df118372ce5127b69
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-11 11:10:24 +0200

    Update objectEvasion.cpp

--- Mindstorm/Adoptoro_Roboto/objectEvasion.cpp
+++ Mindstorm/Adoptoro_Roboto/objectEvasion.cpp
@@ -66,7 +66,7 @@ void evadeObject(BrickPi3 &BPEva, borderValues &calibratedInputs){
     BPEva.set_motor_position_relative(PORT_B, 0); // Set right wheel to stop
     BPEva.set_motor_position_relative(PORT_C, 0); // Set left wheel to stop
     cout << "heloo its me";
-    turnHead90degrees(BPEva, rotateRight);
+    turnHead90degrees(BPEva, rotateLeft);
     turnCar(BPEva, rotateLeft);
     sleep(500);
 }
