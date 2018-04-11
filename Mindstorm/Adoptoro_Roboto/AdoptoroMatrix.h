679ef742d57d18e6e5f52a82bbc507e64a55c15c
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-11 12:47:26 +0200

    Update AdoptoroMatrix.h
    Slashed out some functions to test bot

--- Mindstorm/Adoptoro_Roboto/AdoptoroMatrix.h
+++ Mindstorm/Adoptoro_Roboto/AdoptoroMatrix.h
@@ -20,9 +20,9 @@ using namespace std;
 	
 //};
 
-getDist(BrickPi3 &BPEva); //gets the distance of the nearest object infront of him
+//getDist(BrickPi3 &BPEva); //gets the distance of the nearest object infront of him
 
-turnAround(BrickPi3 &BPMatrix, int pos); // turns the robot around to last crossroad
+//turnAround(BrickPi3 &BPMatrix, int pos); // turns the robot around to last crossroad
 
 borderValues sensorCalibration(BrickPi3 &BPMain); // Calibrates Sensors
 
