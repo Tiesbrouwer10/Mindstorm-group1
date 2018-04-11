90825c42e9bf0b12286cacd1fc799d531e097c37
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-11 12:44:37 +0200

    Update AdoptoroMatrix.h

--- Mindstorm/Adoptoro_Roboto/AdoptoroMatrix.h
+++ Mindstorm/Adoptoro_Roboto/AdoptoroMatrix.h
@@ -13,19 +13,22 @@ using namespace std;
 //==================================
 // Header content
 
-struct borderValues{ // Contains to be updated border values from calibration
+//struct borderValues{ // Contains to be updated border values from calibration
 	
-	int borderValueBW;
-	int borderValueC;
+//	int borderValueBW;
+//	int borderValueC;
 	
-};
+//};
 
+getDist(BrickPi3 &BPEva); //gets the distance of the nearest object infront of him
+
+turnAround(BrickPi3 &BPMatrix, int pos); // turns the robot around to last crossroad
 
 borderValues sensorCalibration(BrickPi3 &BPMain); // Calibrates Sensors
 
 void exitSignalHandler(int signo); // Exits the program with no run-away motors
 
-void MatrixRider(borderValues calibratedInputs, BrickPi3 & BPMatrix); // Follows matrix
+void matrixRider(borderValues calibratedInputs, BrickPi3 & BPMatrix); // Follows matrix
 
 void turning(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, borderValues calibratedInputs); // turning the robot
 
