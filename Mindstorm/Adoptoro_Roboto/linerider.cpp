fe50e7a77d877ae650060061a1d934df8530605d
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-11 10:04:14 +0200

    Update linerider.cpp

--- Mindstorm/Adoptoro_Roboto/linerider.cpp
+++ Mindstorm/Adoptoro_Roboto/linerider.cpp
@@ -64,7 +64,7 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 }
 
 
-void lineSeen(uint8_t insideMotor, uint8_t outsideMotor){
+void lineSeen(uint8_t insideMotor, uint8_t outsideMotor, float Accelerator){
 	if(Accelerator > 69){
 		BPLine.set_motor_power(insideMotor, -60);
 	}else{
