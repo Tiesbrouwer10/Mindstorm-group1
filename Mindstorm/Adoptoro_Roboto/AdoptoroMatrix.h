0654159ff86210c67feb89ad0a7d1f102e4b75bf
Maerecque <33025408+Maerecque@users.noreply.github.com>
2018-04-12 09:56:38 +0200

    Update AdoptoroMatrix.h

--- Mindstorm/Adoptoro_Roboto/AdoptoroMatrix.h
+++ Mindstorm/Adoptoro_Roboto/AdoptoroMatrix.h
@@ -32,10 +32,12 @@ void exitSignalHandler(int signo); // Exits the program with no run-away motors
 
 void matrixRider(borderValues calibratedInputs, BrickPi3 & BPMatrix); // Follows matrix
 
-void turning(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, borderValues calibratedInputs); // turning the robot
+void turning(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, int calibratedInputs, int sensor); // turning the robot
 
-void lineSeen(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, unsigned int &objectTimer, BrickPi3 &BPLine);//Turning function
+void lineSeenM(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, unsigned int &objectTimer, BrickPi3 &BPLine);//Turning function
 
 void sound(); // Start sound effect
+
+void riding(int &Pos, uint8_t PORT_B, uint8_t PORTC, int &Accelerator, BrickPi3 &BPMatrix, int orientation, int borderValueBW, int borderValueC);//Drives forward
 
 #endif /* MATRIX_HPP */
