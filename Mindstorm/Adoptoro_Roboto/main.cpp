5abc72d05ff7fefc9b2c6762297e23811b2ae390
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 09:53:08 +0200

    added matrix

--- Mindstorm/Adoptoro_Roboto/main.cpp
+++ Mindstorm/Adoptoro_Roboto/main.cpp
@@ -14,14 +14,22 @@ BrickPi3 BPMain;
 
 int main(){
 
+	char modus;
+	
 	signal(SIGINT, exitSignalHandler); // Register the exit function for Ctrl+C0
 	
 	BPMain.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers
 	//sound(); // For fun
 	// Get values from sensors
 	// sensorCalibration();
-	
+	cout << "GEEF MODUS ROBOT, L voor line, M voor Matrix";
+	cin >> modus;
 	// Start lineriding function
-	lineRider(sensorCalibration(BPMain), BPMain);
+	if(modus == 'L'){
+		lineRider(sensorCalibration(BPMain), BPMain);
+	}
+	else if(modus == 'M'){
+		matrixRider(sensorCalibration(BPMain), BPMain);
+	}
 	
 }
