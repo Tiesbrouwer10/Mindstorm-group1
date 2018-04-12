d490f333c38e59a290cbaf5f6b57385dd0cc93f5
NielsRisseeuw <31854618+NielsRisseeuw@users.noreply.github.com>
2018-04-12 20:13:13 +0200

    Update objectEvasion.cpp

--- Mindstorm/Adoptoro_Roboto/objectEvasion.cpp
+++ Mindstorm/Adoptoro_Roboto/objectEvasion.cpp
@@ -46,7 +46,7 @@ void turnCar(BrickPi3 &BPEva, int rotateDirection){
     BPEva.set_motor_power(PORT_C, (motorPower * -1)); 
     BPEva.set_motor_power(PORT_B, motorPower);
 	
-    while(BPEva.get_motor_encoder(PORT_C) <= 494 && BPEva.get_motor_encoder(PORT_C) >= -494){ // Rotates it until desired rotation is met (90)
+    while(BPEva.get_motor_encoder(PORT_C) <= 499 && BPEva.get_motor_encoder(PORT_C) >= -499){ // Rotates it until desired rotation is met (90)
 	    sleep(0.01);
     }
 	
