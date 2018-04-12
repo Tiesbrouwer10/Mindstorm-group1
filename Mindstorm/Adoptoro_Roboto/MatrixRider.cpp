04cb1398b3f0d4c2d693e0169245de4744055f73
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 12:42:26 +0200

    Update MatrixRider.cpp

--- Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
+++ Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
@@ -39,8 +39,8 @@ void matrix(borderValues calibratedInputs, BrickPi3 &BPMatrix){
 void riding(uint8_t rightMotor, uint8_t leftMotor, float &Accelerator, BrickPi3 &BPMatrix){
 	
 }
-/*
+
 void lineSeenM(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, BrickPi3 &BPMatrix){
 	
 }
-*/
+
