c1ef901fdc053f890a534c770ed5845238c48c40
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 12:48:13 +0200

    Update MatrixRider.cpp

--- Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
+++ Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
@@ -37,10 +37,37 @@ void matrix(borderValues calibratedInputs, BrickPi3 &BPMatrix){
 }
 
 void riding(uint8_t rightMotor, uint8_t leftMotor, float &Accelerator, BrickPi3 &BPMatrix){
-	
+	     if(CLine < borderValueC && BWLine > borderValueBW){
+         if(orientation == 0 || orientation == 1){
+            Pos++;
+         }
+         else{
+            Pos--;
+         }
+     }
+     else if(BWLine > borderValueBW){
+		lineSeenM(leftMotor, rightMotor, Accelerator, BPLine);
+    }
+    else if(CLine < borderValueC){
+		lineSeenM(rightMotor, leftMotor, Accelerator, BPLine);
+	}
+	else{
+		BPMatrix.set_motor_power(PORT_C, 40);
+		BPMatrix.set_motor_power(PORT_B, 40);
+		Accelerator = 40;
+    }
 }
 
 void lineSeenM(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, BrickPi3 &BPMatrix){
-	
+		if(Accelerator > 69){
+		BPMatrix.set_motor_power(insideMotor, -60);
+	}else{
+		BPMatrix.set_motor_power(insideMotor, 15);
+		BPMatrix.set_motor_power(outsideMotor, Accelerator);
+	}
+	if(Accelerator <= 70){
+		Accelerator += 0.2;
+		sleep(0.01);
+	}
 }
 
