097b8b72e361dff64faa9d99831d41b8ede2529d
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 09:49:34 +0200

    Trial and error for the matrix

--- Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
+++ Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
@@ -8,59 +8,92 @@
 
 using namespace std; 
 
-void matrix(borderValues calibratedInputs){
-    
-    BrickPi3 BPMatrix;
-    
-    BPMatrix.set_sensor_type(PORT_2, SENSOR_TYPE_NXT_LIGHT_ON);
-    BPMatrix.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_COLOR_FULL);
+void matrixRider(borderValues calibratedInputs, BrickPi3 &BPMatrix){
     
     sensor_light_t  Light;
     sensor_color_t  Color;
     
     int posX = 0;
     int posY = 0;
+    int CLine = 0;
+    int BWline = 0;
+    int distanceToObject = 0;
+    float Accelerator = 40
+    int orientation = 0;
     
-
-    while (true){
-        // Get value from sensors
+    while(posX != 4 && posY != 4){
         if(BPLine.get_sensor(PORT_2, Light) == 0){
-            BWLine = Light.reflected;
-            if(BPLine.get_sensor(PORT_3,Color) == 0){
-                CLine = Color.reflected_red;
+			BWLine = Light.reflected;
+			if(BPLine.get_sensor(PORT_3,Color) == 0){
+	            CLine = Color.reflected_red;
                 
-                //controls if X coördinate is the same as the X goal
-                while(posX != 4){
-                    BPLine.set_motor_power(PORT_C, 40);
-                    BPLine.set_motor_power(PORT_B, 40);
-                    if(BWLine > calibratedInputs.borderValueBW &&  CLine < calibratedInputs.borderValueC){
-                        BPLine.set_motor_power(PORT_C, 0);
-                        BPLine.set_motor_power(PORT_B, 0);
-                        X++;
-                    }
+                if(orientation == 0 && posY != 4 ){
+                    riding(posY, PORT_B, PORTC, Accelerator, BPMatrix, orientation, Light.reflected, Color.reflected_red);
+                    cout << "posY\n";
                 }
-                turnRight();
-                while(posY != 4){
-                    BPLine.set_motor_power(PORT_C, 40);
-                    BPLine.set_motor_power(PORT_B, 40);
-                    if(BWLine > calibratedInputs.borderValueBW &&  CLine < calibratedInputs.borderValueC){
-                        BPLine.set_motor_power(PORT_C, 0);
-                        BPLine.set_motor_power(PORT_B, 0);
-                        Y++;
-                    }
+                else if(orientation == 0 && posY == 4){
+                    turning(PORT_B, PORT_C, BPMatrix, Light.reflected, BWLine);
                 }
-                
+                else if(orientation == 1 && posX != 4){
+                    riding(posX, PORT_B, PORTC, Accelerator, BPMatrix, orientation, Light.reflected, Color.reflected_red);
+                }
+                else if(orientation == 2 && posY != 0){
+                    riding(posY, PORT_B, PORTC, Accelerator, BPMatrix, orientation, Light.reflected, Color.reflected_red);
+                }
+                else if(orientation == 3 && posX != 0){
+                    riding(posX, PORT_B, PORTC, Accelerator, BPMatrix, orientation, Light.reflected, Color.reflected_red);
+                } 
             }
         }
-        break;
     }
 }
 
-void turnRight(){
-    BPMatrix.set_motor_power(PORT_B,-40);
-    BPMatrix.set_motor_power(PORT_C, 40);
-    if(BWLine > calibratedInputs.borderValueBW){
-        BPMatrix.set_motor_power(PORT_B,0);
-        BPMatrix.set_motor_power(PORT_C,0);   
+void turning(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, int calibratedInputs, int sensor){
+    
+    BPMatrix.set_motor_power(insideMotor,-40);
+    BPMatrix.set_motor_power(outsideMotor, 40);
+    if(sensor > calibratedInputs){
+        BPMatrix.set_motor_power(insideMotor,0);
+        BPMatrix.set_motor_power(outsideMotor,0); 
+        
     }
+}
+
+int turnAround(BrickPi3 &BPMatrix, int pos){
+       
+}
+
+void riding(int &Pos, uint8_t PORT_B, uint8_t PORTC, int &Accelerator, BrickPi3 &BPMatrix, int orientation, int borderValueBW, int borderValueC){
+     if(CLine < borderValueC && BWLine > borderValueBW){
+         if(orientation == 0 || orientation == 1){
+            Pos++;
+         }
+         else(){
+            Pos--;
+         }
+     }
+     else if(BWLine > borderValueBW){
+		lineSeenM(PORT_C, PORT_B, Accelerator, BPLine);
+    }
+    else if(CLine < borderValueC){
+		lineSeenM(PORT_B, PORT_C, Accelerator, BPLine);
+	}
+	else{
+		BPMatrix.set_motor_power(PORT_C, 40);
+		BPMatrix.set_motor_power(PORT_B, 40);
+		Accelerator = 40;
+    }
+}
+                
+void lineSeenM(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, BrickPi3 &BPMatrix){
+	if(Accelerator > 69){
+		BPMatrix.set_motor_power(insideMotor, -60);
+	}else{
+		BPMatrix.set_motor_power(insideMotor, 15);
+		BPMatrix.set_motor_power(outsideMotor, Accelerator);
+	}
+	if(Accelerator <= 70){
+		Accelerator += 0.2;
+		sleep(0.01);
+	}
 }
