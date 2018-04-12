5882ba73b505c2cec519a2e450a8c43abddfc23d
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 10:18:59 +0200

    Update AdoptoroMatrix.h

--- Mindstorm/Adoptoro_Roboto/AdoptoroMatrix.h
+++ Mindstorm/Adoptoro_Roboto/AdoptoroMatrix.h
@@ -13,7 +13,7 @@ using namespace std;
 //==================================
 // Header content
 
-struct borderValues{ // Contains to be updated border values from calibration
+struct borderValuesMatrix{ // Contains to be updated border values from calibration
 	
 	int borderValueBW;
 	int borderValueC;
@@ -26,7 +26,7 @@ struct borderValues{ // Contains to be updated border values from calibration
 
 
 
-borderValues sensorCalibration(BrickPi3 &BPMain); // Calibrates Sensors
+borderValuesMatrix sensorCalibration(BrickPi3 &BPMain); // Calibrates Sensors
 
 void exitSignalHandler(int signo); // Exits the program with no run-away motors
 
