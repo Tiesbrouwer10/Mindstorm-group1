3a2393cb56dbf1adad77adba21de0c2734e5b13b
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 14:31:07 +0200

    Update MatrixRider.cpp

--- Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
+++ Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
@@ -22,13 +22,13 @@ void matrix(borderValues calibratedInputs, BrickPi3 &BPMatrix){
  	float Accelerator = 40;
  	int orientation = 0;
  
- 	while(posX != 4 && posY != 4){
+ 	while(posX != 4 || posY != 4){
      		if(BPMatrix.get_sensor(PORT_2, Light) == 0){
 	       		BWLine = Light.reflected;
 	          	if(BPMatrix.get_sensor(PORT_3,Color) == 0){
 	             		CLine = Color.reflected_red;
                 
-              			if(orientation == 0 && posY != 4 ){
+              			if(posY != 4 ){
                 			riding(PORT_B, PORT_C, Accelerator, BPMatrix, CLine, BWLine, calibratedInputs.borderValueC, calibratedInputs.borderValueBW, orientation, posY);
 					posY++;
 					cout << posY << " :DE Y PLEK\n";
