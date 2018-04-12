f7ae4843b7cf84a76ce2634d4df9c81f867e3f54
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 15:15:18 +0200

    Update linerider.cpp

--- Mindstorm/Adoptoro_Roboto/linerider.cpp
+++ Mindstorm/Adoptoro_Roboto/linerider.cpp
@@ -30,7 +30,7 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 			BWLine = Light.reflected;
 			if(BPLine.get_sensor(PORT_3,Color) == 0){
 				CLine = Color.reflected_red;
-				
+				// if both the color and black/white sensors see something that is not white he ignores it and continues driving
 				if(CLine < calibratedInputs.borderValueC && BWLine > calibratedInputs.borderValueBW){
 					BPLine.set_motor_power(PORT_C, 40);
 					BPLine.set_motor_power(PORT_B, 40);
@@ -42,13 +42,16 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 				
 				// Ride for 1 step
 				if(BWLine > calibratedInputs.borderValueBW){
+					// if the value of the Black/White sensor is higher than the value it has as white 
 					lineSeen(PORT_C, PORT_B, Accelerator, objectTimer, BPLine);
 				}
 				else if(CLine < calibratedInputs.borderValueC){
+					//if the value of the color sensor is lower than the value it has as white
 					lineSeen(PORT_B, PORT_C, Accelerator, objectTimer, BPLine);
 
 				}
 				else{
+					// set a base speed for the robot if it rides a straight line
 					BPLine.set_motor_power(PORT_C, 40);
 					BPLine.set_motor_power(PORT_B, 40);
 					Accelerator = 40;
@@ -60,6 +63,16 @@ void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine){
 		}
 	}
 }
+/*
+
+uint8_t insideMotor = The motor thats on the inside when turning
+uint8_t outsideMotor = The motor thats on the outside when turning
+float &Accelerator = 
+unsigned int &objectTimer = 
+BrickPi3 &BPLine = 
+
+*/
+
 
 void lineSeen(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, unsigned int &objectTimer, BrickPi3 &BPLine){
 	if(Accelerator > 69){
