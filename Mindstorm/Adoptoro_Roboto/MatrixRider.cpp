086a34ee0fa25c61e09349acde69e8bed8abaaac
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 10:56:24 +0200

    Update MatrixRider.cpp

--- Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
+++ Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
@@ -1,1 +1,15 @@
 
+//==================================
+// Headerfile includes
+
+#include "BrickPi3.h"
+#include "AdoptoroLijn.h"
+#include "AdoptoroMatrix.h"
+
+//==================================
+
+using namespace std; 
+
+void matrixRider(borderValues calibratedInputs, BrickPi3 &BPMatrix){
+  cout << calibratedInput.borderValueBW << '\n';
+}
