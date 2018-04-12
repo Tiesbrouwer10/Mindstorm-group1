f01500cd621be6accf0b300c8872724b6a72becc
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 11:42:02 +0200

    Update AdoptoroLijn.h

--- Mindstorm/Adoptoro_Roboto/AdoptoroLijn.h
+++ Mindstorm/Adoptoro_Roboto/AdoptoroLijn.h
@@ -51,11 +51,7 @@ void sound(); // Start sound effect
 
 void matrix(borderValues calibratedInputs, BrickPi3 &BPMatrix);
 
-void lineSeenM(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, BrickPi3 &BPMatrix);
 
-void riding(int &Pos, uint8_t PORT_B, uint8_t PORTC, int &Accelerator, BrickPi3 &BPMatrix, int orientation, int borderValueBW, int borderValueC);
-
-void turning(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, int calibratedInputs, int sensor);
 
 //void objectDetection(BrickPi3 BPLine, unsigned int &objectTimer, borderValues calibratedInputs); // Check if object is within range
 
