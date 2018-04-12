0ba01fdc65ea21c263ec95b76ee4275f81897e59
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 20:11:43 +0200

    changed return to break

--- Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
+++ Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
@@ -76,7 +76,7 @@ void riding(uint8_t rightMotor, uint8_t leftMotor, float &Accelerator, BrickPi3 
 		if(CLine < borderValueC && BWLine > borderValueBW){
      			cout << "ZIE EEN KRUISPUNT\n";
 			Pos++;
-			return;
+			break;
 
      		}
 		// if the black/white sensor doesn't measure white
