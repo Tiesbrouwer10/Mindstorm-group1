54cd36f912a5e7ace938ee166c641a995acdc3bb
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-12 11:29:13 +0200

    Update MatrixRider.cpp

--- Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
+++ Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
@@ -47,3 +47,47 @@ void matrix(borderValues calibratedInputs, BrickPi3 &BPLine){
       }
    }
 }
+
+void turning(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, int calibratedInputs, int sensor){  
+    BPMatrix.set_motor_power(insideMotor,-40);
+    BPMatrix.set_motor_power(outsideMotor, 40);
+    if(sensor > calibratedInputs){
+        BPMatrix.set_motor_power(insideMotor,0);
+        BPMatrix.set_motor_power(outsideMotor,0);    
+    }
+}
+
+void riding(int &Pos, uint8_t PORT_B, uint8_t PORTC, int &Accelerator, BrickPi3 &BPMatrix, int orientation, int borderValueBW, int borderValueC){
+     if(CLine < borderValueC && BWLine > borderValueBW){
+         if(orientation == 0 || orientation == 1){
+            Pos++;
+         }
+         else{
+            Pos--;
+         }
+     }
+     else if(BWLine > borderValueBW){
+		lineSeenM(PORT_C, PORT_B, Accelerator, BPLine);
+    }
+    else if(CLine < borderValueC){
+		lineSeenM(PORT_B, PORT_C, Accelerator, BPLine);
+	}
+	else{
+		BPMatrix.set_motor_power(PORT_C, 40);
+		BPMatrix.set_motor_power(PORT_B, 40);
+		Accelerator = 40;
+    }
+}
+                
+void lineSeenM(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, BrickPi3 &BPMatrix){
+	if(Accelerator > 69){
+		BPMatrix.set_motor_power(insideMotor, -60);
+	}else{
+		BPMatrix.set_motor_power(insideMotor, 15);
+		BPMatrix.set_motor_power(outsideMotor, Accelerator);
+	}
+	if(Accelerator <= 70){
+		Accelerator += 0.2;
+		sleep(0.01);
+	}
+}
