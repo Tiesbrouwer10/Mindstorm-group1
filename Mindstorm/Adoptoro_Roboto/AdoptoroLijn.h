5a13b26a9152e81a7e94e0336f2da0922e4b7844
Tonyvdk <t.r.vanderkrogt@gmail.com>
2018-04-13 12:35:46 +0200

    ordered the header file

--- Mindstorm/Adoptoro_Roboto/AdoptoroLijn.h
+++ Mindstorm/Adoptoro_Roboto/AdoptoroLijn.h
@@ -26,42 +26,60 @@ struct borderValues{ // Contains to be updated border values from calibration
 //==================================
 // Header content
 
-borderValues sensorCalibration(BrickPi3 &BPMain); // Calibrates Sensors
+//NEEDED FOR BOTH
 
-void exitSignalHandler(int signo); // Exits the program with no run-away motors
+// Calibrates Sensors
+borderValues sensorCalibration(BrickPi3 &BPMain); 
 
-void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine); // Follows line, (Currently called from main for 1 step)
+// Exits the program with no run-away motors
+void exitSignalHandler(int signo); 
 
-int getDist(BrickPi3 &BPEva); // Gets the distance the robot is away from object
-
-void evadeObject(BrickPi3 &BPEva, borderValues &calibratedInputs); // Procedure to evading object
-
-void turnHead90degrees(BrickPi3 &BPEva, int rotateDirection); // Rotates the sonar sensor 90 degrees, 1 for left, 2 for right
-
-void turnCar(BrickPi3 &BPEva, int rotateDirection); // Rotates the car 90 degrees, 1 for left, 2 for right
-
-void lineSeen(uint8_t insideMotor, uint8_t outsideMotor, int &Accelerator, unsigned int &objectTimer, BrickPi3 &BPLine); //if the robor drives and sees a line it changes the motor speed to adjust the robot
-
-void drivePastObject(BrickPi3 &BPEva, borderValues calibratedInputs, bool detectLine, bool objectFound, bool &foundLine); // Driving past object and detecting object to turn past
-
-bool findLine(borderValues calibratedInputs, BrickPi3 &BPEva, bool &foundLine); // Checks if the robot as found the line from objectevasion
-
-void returnToLine(BrickPi3 &BPEva, borderValues calibratedInputs, int rotateRight, int rotateLeft); // Returns bot on top of line to continue line riding
-
-void sound(); // Start sound effect
+// Gets the distance the robot is away from object
+int getDist(BrickPi3 &BPEva);
 
 
+
+//EXCLUSIVE TO ONLY LINE
+
+// Follows line, (Currently called from main for 1 step)
+void lineRider(borderValues calibratedInputs, BrickPi3 &BPLine);
+
+// Procedure to evading object
+void evadeObject(BrickPi3 &BPEva, borderValues &calibratedInputs); 
+
+// Rotates the sonar sensor 90 degrees, 1 for left, 2 for right
+void turnHead90degrees(BrickPi3 &BPEva, int rotateDirection); 
+
+// Rotates the car 90 degrees, 1 for left, 2 for right
+void turnCar(BrickPi3 &BPEva, int rotateDirection); 
+
+//if the robor drives and sees a line it changes the motor speed to adjust the robot
+void lineSeen(uint8_t insideMotor, uint8_t outsideMotor, int &Accelerator, unsigned int &objectTimer, BrickPi3 &BPLine); 
+
+// Driving past object and detecting object to turn past
+void drivePastObject(BrickPi3 &BPEva, borderValues calibratedInputs, bool detectLine, bool objectFound, bool &foundLine); 
+
+// Checks if the robot as found the line from objectevasion
+bool findLine(borderValues calibratedInputs, BrickPi3 &BPEva, bool &foundLine); 
+
+// Returns bot on top of line to continue line riding
+void returnToLine(BrickPi3 &BPEva, borderValues calibratedInputs, int rotateRight, int rotateLeft); 
 
 //EXCLUSIVE TO MATRIX
 
-void matrix(borderValues calibratedInputs, BrickPi3 &BPMatrix); // Starts the matrix code
+// Starts the matrix code
+void matrix(borderValues calibratedInputs, BrickPi3 &BPMatrix); 
 
-void lineSeenM(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, BrickPi3 &BPMatrix); // The robot reposition itself until its correctly on the line
+// The robot reposition itself until its correctly on the line
+void lineSeenM(uint8_t insideMotor, uint8_t outsideMotor, float &Accelerator, BrickPi3 &BPMatrix); 
 
-void riding(uint8_t rightMotor, uint8_t leftMotor, float &Accelerator, BrickPi3 &BPMatrix, int borderValueC, int borderValueBW, int &Pos, sensor_light_t &Light, sensor_color_t &Color, int &orientation); //Checks what his position on the line is
-	
-void turnRight(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, int calibratedInputs, sensor_light_t &Light, int &orientation); // The robot turns to the right until it sees the black line again
+//Checks what his position on the line is
+void riding(uint8_t rightMotor, uint8_t leftMotor, float &Accelerator, BrickPi3 &BPMatrix, int borderValueC, int borderValueBW, int &Pos, sensor_light_t &Light, sensor_color_t &Color, int &orientation); 	
 
-void turnLeft(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, int calibratedInputs, sensor_color_t &Color, int &orientation); // The robot turns to the left until it sees the black line again
+// The robot turns to the right until it sees the black line again
+void turnRight(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, int calibratedInputs, sensor_light_t &Light, int &orientation); 
+
+// The robot turns to the left until it sees the black line again
+void turnLeft(uint8_t insideMotor, uint8_t outsideMotor, BrickPi3 &BPMatrix, int calibratedInputs, sensor_color_t &Color, int &orientation); 
 
 #endif /* LIJN_HPP */
