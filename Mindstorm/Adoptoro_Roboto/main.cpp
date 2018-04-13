348c9d359bae4b0a05adc4d462bdbab611a1d06b
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2018-04-13 12:27:39 +0200

    Added comments

--- Mindstorm/Adoptoro_Roboto/main.cpp
+++ Mindstorm/Adoptoro_Roboto/main.cpp
@@ -16,18 +16,19 @@ int main(){
 
 	char modus;
 	
-	signal(SIGINT, exitSignalHandler); // Register the exit function for Ctrl+C0
-	
+	// Struct setup and exit handler
+	signal(SIGINT, exitSignalHandler); // Register the exit function for Ctrl+C
 	BPMain.detect(); // Make sure that the BrickPi3 is communicating and that the firmware is compatible with the drivers
-	//sound(); // For fun
+	
 	// Get values from sensors
-	// sensorCalibration();
 	cout << "GEEF MODUS ROBOT, L voor line, M voor Matrix: ";
 	cin >> modus;
+	
 	// Start lineriding function
 	if(modus == 'L'){
 		lineRider(sensorCalibration(BPMain), BPMain);
 	}
+	// Start matrix function
 	else if(modus == 'M'){
 		matrix(sensorCalibration(BPMain), BPMain);
 	}
