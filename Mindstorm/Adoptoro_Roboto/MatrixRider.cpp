35eb9f5ed17e985a8c284b7fada7f02add691c7b
jasperdewinther <31817802+jasperdewinther@users.noreply.github.com>
2018-04-13 09:12:00 +0200

    added test cout for position

--- Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
+++ Mindstorm/Adoptoro_Roboto/MatrixRider.cpp
@@ -37,7 +37,7 @@ void matrix(borderValues calibratedInputs, BrickPi3 &BPMatrix){
 	       		BWLine = Light.reflected;
 	          	if(BPMatrix.get_sensor(PORT_3,Color) == 0){
 	             		CLine = Color.reflected_red;
-                
+                		cout << posY << ", " << posX << "\n";
               			if(posY != 3 ){
 					
                 			riding(PORT_B, PORT_C, Accelerator, BPMatrix, CLine, BWLine, calibratedInputs.borderValueC, calibratedInputs.borderValueBW, orientation, posY);																					
